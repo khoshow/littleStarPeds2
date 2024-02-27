b2f5c0cd5c963d75232ad8ca5f032876f173762f
khoshow <khoshow.official@gmail.com>
2024-02-27 21:08:30 +0530

    gtag

--- pages/_document.js
+++ pages/_document.js
@@ -9,16 +9,18 @@ export default function Document() {
           name="description"
           content="Little Star Pediatrics - Modern, Innovative Pediatric Care"
         />
-        <meta
-          name="keywords"
-          content=""
-        />
-
+        <meta name="keywords" content="" />
         <link rel="icon" href="/favicon.jpg" />
         <Script
           src="https://kit.fontawesome.com/4af13880de.js"
           crossOrigin="anonymous"
         ></Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-WSP1FR31HT"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }}
+        ></noscript>
       </Head>
       <body>
         <Main />
