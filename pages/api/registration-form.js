e6f7550b9be21b5aa1d95545f2f9f8384e6eaa7b
khoshow <khoshow.official@gmail.com>
2024-03-29 21:39:39 +0530

    emails

--- pages/api/registration-form.js
+++ pages/api/registration-form.js
@@ -260,10 +260,10 @@ const submitRegistration = (req, res, next) => {
             to: [
               {
                 // email: "khoshow@gmail.com",
-                email: "info@littlestarpeds.com"
+                email: "info@littlestarpeds.com", email: req.body.email2
               },
             ],
-            cc: [{ email: "littlestarpediatrics@gmail.com",  }],
+            cc: [{ email: "littlestarpediatrics@gmail.com",   }],
             bcc: [
               {
                 email: "khoshow.official@gmail.com",
