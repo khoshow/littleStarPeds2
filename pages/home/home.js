39ffc7885f2b53969198dd46967f9dbc1b97323e
khoshow <khoshow.official@gmail.com>
2024-12-22 19:36:27 +0530

    holiday

--- pages/home/home.js
+++ pages/home/home.js
@@ -73,7 +73,7 @@ const Insurance = () => {
 				Feature Section Thirty Four
 			============================================== 
 			--> */}
-      {/* <div style={{ backgroundColor: "#29aae1", padding: "1rem" }}>
+      <div style={{ backgroundColor: "#29aae1", padding: "1rem" }}>
         <div
           className="card col-md-4 alert-success"
           style={{ marginLeft: "auto", marginRight: "auto" }}
@@ -84,10 +84,11 @@ const Insurance = () => {
             </h5>
 
             <p style={{ color: "#007BFF" }}>
-              <span className="message">New</span> In observance of Independence
-              Day, clinics will be closed on July 4th & 5th. Happy holiday!
-            </p> 
-          <p style={{ color: "#007BFF" }}><span className="message">New</span> In observance of the Labor Day Holiday, clinic will remain closed on 5th September, Monday. </p>
+              <span className="message">New</span> Clinics will be closed on
+              December 24th, 25th & 31st 2024 and Januray 1st 2025. Happy
+              Holiday!
+            </p>
+            {/* <p style={{ color: "#007BFF" }}><span className="message">New</span> In observance of the Labor Day Holiday, clinic will remain closed on 5th September, Monday. </p>
             <a className="card-link" href="./files/Covid 19.pdf">Updates on Covid-19</a
             ><br /><br />
             <a
@@ -103,10 +104,10 @@ const Insurance = () => {
             >
               <span className="message">New</span>
               Link to Similac Formula recall information for parents</a
-            > 
+            >  */}
           </div>
         </div>
-      </div> */}
+      </div>
       {/* <div style={{ backgroundColor: "#f00b42", padding: "1rem" }}>
         <div
           className="card col-md-4 alert-success"
